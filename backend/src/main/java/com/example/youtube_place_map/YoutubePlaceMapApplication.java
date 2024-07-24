69e521924fb71c662ce23478987f4f5fca600121
YouVin <dbqls990909@naver.com>
2024-07-24 23:16:38 +0900

    프론트엔드 백엔드 설정

---
+++ backend/src/main/java/com/example/youtube_place_map/YoutubePlaceMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.youtube_place_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubePlaceMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubePlaceMapApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/youtube_place_map/YoutubePlaceMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.youtube_place_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubePlaceMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
